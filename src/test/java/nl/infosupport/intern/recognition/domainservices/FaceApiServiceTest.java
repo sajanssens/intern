aaba1086334e06bdd0e909a4a7b6735d279a1bd5
Ricbak <ricobakels2@gmail.com>
2019-04-25 11:22:14 +0200

    ...

--- src/test/java/nl/infosupport/intern/recognition/domainservices/FaceApiServiceTest.java
+++ src/test/java/nl/infosupport/intern/recognition/domainservices/FaceApiServiceTest.java
@@ -1,5 +1,12 @@
+package nl.infosupport.intern.recognition.domainservices;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FaceApiServiceTest {
 
+    @Test
+    void createPerson() {
+    }
 }
