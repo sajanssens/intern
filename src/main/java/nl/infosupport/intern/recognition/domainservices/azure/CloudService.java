ce51401af198b54ad03356edc3d9da482d4bfc1f
Ricbak <ricobakels2@gmail.com>
2019-04-26 08:47:50 +0200

    small change

--- src/main/java/nl/infosupport/intern/recognition/domainservices/azure/CloudService.java
+++ src/main/java/nl/infosupport/intern/recognition/domainservices/azure/CloudService.java
@@ -1,8 +1,10 @@
 package nl.infosupport.intern.recognition.domainservices.azure;
 
+import java.awt.image.BufferedImage;
+
 public interface CloudService {
 
     String createPerson(String name);
 
-//    String addFaceToPerson(String personId, BufferedImage image);
+    String addFaceToPerson(String personId, BufferedImage image);
 }
